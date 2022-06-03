58075ba169d805a45177c882440ff926aa956ba2
Abinash Patri <abinashpatri33@gmail.com>
2022-06-03 16:39:58 +0530

    project update

--- src/containers/portfolio/utils.js
+++ src/containers/portfolio/utils.js
@@ -1,12 +1,28 @@
 import Notesapp from "../../images/notesapp.png";
+import Calc from '../../images/calculator.png';
+import dwnldr from '../../images/downloader.png'
 
 export const portfolioData = [
   {
-    sectionId: 2,
+    sectionId: 3,
     projectName: "Notes Application",
     projectLink1: "https://avisnoteapp.netlify.app/",
     projectLink2: "https://github.com/Abinashpatri/notesapp",
     image: Notesapp,
+  },
+  {
+    sectionId: 2,
+    projectName: "Calculator",
+    projectLink1: "https://abinashpatri.github.io/calculator/",
+    projectLink2: "https://github.com/Abinashpatri/calculator",
+    image: Calc,
+  },
+  {
+    sectionId: 2,
+    projectName: "File Downloader",
+    projectLink1: "https://github.com/Abinashpatri/File-Downloader",
+    projectLink2: "https://abinashpatri.github.io/File-Downloader/",
+    image: dwnldr,
   },
   
 ];
@@ -17,7 +33,12 @@ export const filterOptions = [
     id: 1,
   },
   {
-    label: "Developement",
+    label: "JavaScript",
     id: 2,
-  }
+  },
+  {
+    label: "React",
+    id: 3,
+  },
+  
 ];
